05dfea082f9d99dcc28738f7b4676455e4f68c05
zsyzsy <552068363@qq.com>
2017-06-06 17:24:52 +0800

    新增获取回到顶部、底部的位置

--- zl/小功能/滚动条/js/jrfw1.js
+++ zl/小功能/滚动条/js/jrfw1.js
@@ -2,17 +2,29 @@
  * Created by Administrator on 2017/5/17 0017.
  */
 $(function ($) {
-//点击事件
-
-
+    //实时获取滚动条的方法
     $(window).scroll(function(){
-        // console.log($(window).scrollTop());
+        console.log($(window).scrollTop());//实时获取滚动条的位置
+       //判断滚动条
+        if ($(document).scrollTop()<=0){
+            console.log("滚动条已经到达顶部为0");
+        }
+        // console.log($(document).scrollTop());  //滚动条距离上面的位置
+        //console.log($(document).height() - $(window).height());  //滚动条底部位置
+        if ($(document).scrollTop() >= $(document).height() - $(window).height()) {
+            console.log("滚动条已经到达底部为" + $(document).scrollTop());
+        }
+
+
+        // 滚动条大于430像素时,固定菜单显示,否则隐藏
         if($(window).scrollTop()>430){
             $(".jrfw_topnav").show();
 
         }else{
             $(".jrfw_topnav").hide();
         }
+
+        //获取每块区域滚动条的位置到顶部的距离
         var jrfw_fwfw=$("#jrfw_fwfw").offset().top-50;  //475
         var jrfw_fwlc=$("#jrfw_fwlc").offset().top-50;   //720
         var jrfw_sqcl=$("#jrfw_sqcl").offset().top-50;  //1337
@@ -20,10 +32,10 @@ $(function ($) {
         var jrfw_fwxy=$("#jrfw_fwxy").offset().top;
         var jrfw_fwxy_h=jrfw_fwxy+$('.jrfw_xyBox').height()+$('#jrfw_fwxy').height();
         // console.log(jrfw_fwzxdh);
+
         //滚动移动到相应的位置区间，添加li菜单样式
          if(($(window).scrollTop()>jrfw_fwfw)&&($(window).scrollTop()<jrfw_fwlc)){
-             //console.log($(document).height() - $(window).height());  //滚动条底部位置
-            // console.log($(document).scrollTop());  //滚动条距离上面的位置
+             //(判断滚动条距离上面的位置)<(滚动条底部位置),是就添加class属性,否则什么都不做
              if ($(document).scrollTop() <$(document).height() - $(window).height()) {
                  $(".jrfw_mainbav  .jrfw_fwfw").addClass("lis").siblings().removeClass("lis");
              }
@@ -54,20 +66,20 @@ $(function ($) {
 //点击菜单，滚动到指定位置
     $('.jrfw_mainbav  li,.jrfw_topnav .jrfw_mainbav li').each(function() {
         $(this).click(function() {
-            // $(".jrfw_mainbav li").removeClass("lis");
             var href1=$(this).attr('class');
-            $("."+href1).addClass("lis").siblings().removeClass("lis");
+            $("."+href1).addClass("lis").siblings().removeClass("lis");  //添加class属性,并找到该元素的同胞元素移除class属性
             // // console.log(href1);
             var scrollT=$("#"+href1).offset().top-30;
             // // console.log(scrollT);
             $("body").scrollTop(scrollT);
         })
     });
-    //默认回到底部
+    //默认回到最后一块区域的滚动位置
     var scrollTo=$("#jrfw_fwxy").offset().top-30;
     $(".jrfw_fwxy").addClass("lis").siblings().removeClass("lis");
     $("body").scrollTop(scrollTo);
-    //回到顶部
+
+    //回到顶部按钮
     var offset = 100,
         offset_opacity = 1200,
         scroll_top_duration = 700,
